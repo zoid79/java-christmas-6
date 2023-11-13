f2ee269336dc6f88ff83dc347d37542456321578
zoid79 <zidwkd00@naver.com>
2023-11-13 23:49:50 +0900

    add inputView

---
+++ src/main/java/christmas/UI/InputView.java
@@ -0,0 +1,17 @@
+package christmas.UI;
+
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class InputView {
+    private static final String DATE ="^(?:[1-9]|[12]\\d|3[0-1])$";
+    public int inputDate() {
+        String stringDate = Console.readLine();
+        if(stringDate.matches(DATE)) {
+            System.out.println(stringDate);
+            return Integer.parseInt(stringDate);}
+        throw new IllegalArgumentException("[ERROR] 유효하지 않은 날짜입니다. 다시 입력해 주세요.");
+    }
+}
