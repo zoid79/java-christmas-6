fde57c29b4b29fe5b89ca6514fc8aadb449f5f4e
zoid79 <zidwkd00@naver.com>
2023-11-14 02:52:54 +0900

    fin Order

--- src/main/java/christmas/Order/Order.java
+++ src/main/java/christmas/Order/Order.java
@@ -6,62 +6,87 @@ import christmas.Menu.Menu;
 import christmas.UI.InputView;
 import christmas.UI.OutputView;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 public class Order {
     InputView inputView;
     OutputView outputView;
     List<Menu> menus;
-    int totalPrice=0;
+    Calculation calculation;
+    private Map<String, Menu> menuMap = new HashMap<>();
+
     public Order() {
+        for(Menu menu : Menu.values()){menuMap.put(menu.getMenuName(), menu);}
         inputView = new InputView();
         outputView= new OutputView();
         menus = new ArrayList<Menu>();
     }
-
     public void start() {
         boolean flag =true;
-        outputView.showFirstMessage();
-        int i=-1;
-        while(flag){
-            try{
-                i=inputView.inputDate();
+        int date=-1;
+        while(flag) {
+            try {
+                outputView.showFirstMessage();
+                date = inputView.inputDate();
                 flag = false;
-            }catch (IllegalArgumentException e){
+            } catch (IllegalArgumentException e) {
                 System.out.println(e.getMessage());
+                System.out.println(flag);
                 flag = true;
             }
+        }
             Menu menu = null;
             flag = true;
             while(flag){
+                outputView.showOrderExample();
+                String order = Console.readLine();
+                System.out.println(order);
                 try{
-                    outputView.showOrderExample();
-                    String order = Console.readLine();
                     String[] menus = order.split("[,\\-]");
                     for(int j=0;j< menus.length;j++){
                         if(j%2==0)menu = getMenu(menus[j]);
-                        if(j%2==1) addMenu(menus[j], menu);
+                        if(j%2==1)addMenu(menus[j], menu);
                     }
-                }catch (IllegalArgumentException e){System.out.println(e.getMessage());}
+                    flag=false;
+                }catch (IllegalArgumentException e){
+                    System.out.println(e.getMessage());
+                    flag=true;
+                }
             }
-            Calculation calculation = new Calculation(this.menus);
-            calculation.calculate();
+            this.outputView.showOrder();
+            int totalPrice = 0;
+
+            calculation = new Calculation(this.menus);
+            try{
+                totalPrice=calculation.countMenu();
+            }catch (IllegalArgumentException e){
+                System.out.println(e.getMessage());
+                return;
             }
+            if(totalPrice<10000){
+                this.outputView.showNoSaleUI(totalPrice);
+                return;
+            }
+            calculation.calculate(date,totalPrice);
+    }
 
 
-        }
+
 
     private void addMenu(String num, Menu menu) {
-        if(num.matches("^[1-9]\\d*$")){
-            for(int k = 0; k<Integer.parseInt(num); k++) this.menus.add(menu);
-        }
-        throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        this.outputView.addCountText(num);
+        if(!num.matches("^[1-9]\\d*$"))throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        for(int k = 0; k<Integer.parseInt(num); k++) this.menus.add(menu);
     }
 
     private Menu getMenu(String menus) {
         Menu menu = null;
-        menu = Menu.getMainMenu(menus);
+        menu = this.menuMap.get(menus);
+        this.menus.remove(menu);
         if(menu==null)throw new IllegalArgumentException("[ERROR] 유효하지 않은 주문입니다. 다시 입력해 주세요.");
+        this.outputView.addMenuText(menu.getMenuName());
         return menu;
     }
 }
