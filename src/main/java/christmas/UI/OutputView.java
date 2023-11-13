e661bd118d50fe4b7f7a1d11cdd4a44d2f564a76
zoid79 <zidwkd00@naver.com>
2023-11-13 23:50:05 +0900

    add outputView

---
+++ src/main/java/christmas/UI/OutputView.java
@@ -0,0 +1,11 @@
+package christmas.UI;
+
+public class OutputView {
+    public void showFirstMessage() {
+        System.out.println("안녕하세요! 우테코 식당 12월 이벤트 플래너입니다.\n" +
+                "12월 중 식당 예상 방문 날짜는 언제인가요? (숫자만 입력해 주세요!)");    }
+
+    public void showOrderExample() {
+        System.out.println("주문하실 메뉴를 메뉴와 개수를 알려 주세요. (e.g. 해산물파스타-2,레드와인-1,초코케이크-1)");
+    }
+}
