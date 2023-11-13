9f937e668f7f300ba5be77a30a05eb414680e2fe
zoid79 <zidwkd00@naver.com>
2023-11-13 23:50:54 +0900

    add Calculation

---
+++ src/main/java/christmas/Calculation.java
@@ -0,0 +1,14 @@
+package christmas;
+
+import christmas.Menu.Menu;
+
+import java.util.List;
+
+public class Calculation {
+    private List<Menu> menus;
+    public Calculation(List<Menu> menus) {this.menus = menus;}
+
+    public void calculate() {
+
+    }
+}
